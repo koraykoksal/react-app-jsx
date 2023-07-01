7f8bc644e0418986a7cc2896457746b66bba3b64
Koray KOKSAL <tkkoksal@icloud.com>
2023-07-02 01:28:14 +0300

    component import export and jsc syntaxt

---
+++ src/Header.js
@@ -0,0 +1,18 @@
+function Header(){
+
+
+    return(
+
+        <>
+        {/* jsx yazımı */}
+
+            <h1>Koray</h1>
+            <h1>Koksal</h1>
+        </>
+
+    )
+
+}
+
+
+export default Header
